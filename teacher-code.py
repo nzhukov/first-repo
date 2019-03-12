d22d2b2c0dd3bd61ea9b7e0a94d212703d24dea4
Nikolai <kkonstantinopolsky@protonmail.com>
2019-03-12 20:42:59 +0300

    Update teacher-code.py

--- teacher-code.py
+++ teacher-code.py
@@ -1,15 +1,14 @@
 import hashlib
 
-# https://docs.python.org/3/library/hashlib.html
-
-
+# https://docs.python.org/3/library/hashlib.html
 # s = b"Hello, world!"
+# s = input("Enter hashed string: ")
+# print(s)
+# hash_func = hashlib.md5()
+# hash_func.update(s)
+# print(hash_func.hexdigest())
 
-s = input("Enter hashed string: ")
-
-print(s)
 
 
-hash_func = hashlib.md5()
-hash_func.update(s)
-print(hash_func.hexdigest())
+
+def main():
